3c0eb1f8b0c0d5d9d31e9ab642502e99906c67c5
J. Farid <jrab227@gmail.com>
2015-08-22 20:31:18 -0400

    Added specs for dataTransforms

--- test/dataTransforms.spec.js
+++ test/dataTransforms.spec.js
@@ -6,25 +6,86 @@ describe("t-chart-d3", function() {
         d3 = require("./../node_modules/d3/d3");
         tChart = TChart(d3);
         mockData = [{'a': 20}, {'a': 10}, {'a': 5}, {'a': 17}, {'a': 9}, {'a': 4}];
+
     });
 
     describe("data transform functions", function() {
         describe("sort function", function() {
-            describe("when given a metric", function() {
+            describe("when given a measure", function() {
+                var measure;
+                beforeEach(function() {
+                    measure = function(a, b) {
+                        return a > b
+                    }
+                })
                 describe("when given data and a property to sort by", function() {
-                    it("should call javascript sort and return results");
+                    var results, by;
+                    beforeEach(function() {
+                        by = "a"
+                        result = tChart.transform.sort(measure, mockData, by)
+                    })
+                    it("should sort ascending from left and return results", function() {
+                        var isSorted = false;
+                        isSorted = mockData.reduce(function(agg, next){
+                            if ((agg.status === false) || (agg.prev < next.a)) {
+                                return false;
+                            }
+                            return {
+                                prev: next.a,
+                                status: true
+                            }
+                        }, {
+                            status: true,
+                            prev: Math.Infinity
+                        })
+                        expect(isSorted).toBeTruthy();
+                    });
                 });
             });
         });
         describe("normalize function", function() {
             describe("when set to normalize by max", function() {
+                var by;
+                beforeEach(function() {
+                    by = "max"
+                })
                 describe("when given data and a property to transform", function() {
-                    it("should normalize data vs max")
+                    var prop, expected, result;
+                    beforeEach(function() {
+                        prop = "a";
+                        expected = [1, .5, .25, 17/20, 9/20, 4/20];
+                        result = tChart.transform.normalize(mockData, by);
+                    })
+                    it("should normalize data vs max", function() {
+                        var vals = result.map(function(val, i){
+                            return val.a === expected[i]
+                        })
+                        var allNormalized = vals.reduce(function(agg, next){
+                            return agg && next
+                        }, true);
+
+                        expect(allNormalized).toBeTruthy();
+                    })
                 })
             })
             describe("when set to normalize by integral", function() {
                 describe("when given data and a property to transform", function() {
-                    it("should normalize data vs sum of data")
+                    var prop, expected, result;
+                    beforeEach(function() {
+                        prop = "a";
+                        expected = [20/65, 10/65, 5/65, 17/65, 9/65, 4/65];
+                        result = tChart.transform.normalize(mockData, by);
+                    })
+                    it("should normalize data vs sum of data", function() {
+                        var vals = result.map(function(val, i){
+                            return val.a === expected[i]
+                        })
+                        var allNormalized = vals.reduce(function(agg, next){
+                            return agg && next
+                        }, true);
+
+                        expect(allNormalized).toBeTruthy();
+                    })
                 })
             })
         })
